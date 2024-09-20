cca86b215064a0b83d71ff2e57a2b5024c1c3221
MengshaLiu <54096652+MengshaLiu@users.noreply.github.com>
2024-09-20 14:10:42 +1000

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -6,7 +6,7 @@ import boto3
 try:
     s3=boto3.client('s3',
                       aws_access_key_id='YOUR_ACCESS_KEY_ID',
-                      aws_secret_access_key='/YOUR_ACCESS_SECRET',
+                      aws_secret_access_key='YOUR_ACCESS_SECRET',
                       region_name='YOUR_REGION')
     consumer = KafkaConsumer('crypto_transactions',
                              bootstrap_servers=['localhost:29092', 'localhost:39092'],
